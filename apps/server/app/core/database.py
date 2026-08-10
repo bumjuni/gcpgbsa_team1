1c131d98237c59d6e8148328e4cbd71d76bd7ecf
JJy <darlian0889@gmail.com>
2026-08-10 15:25:32 +0900

    chore(be): connect CloudSQL MySQL Server

--- apps/server/app/core/database.py
+++ apps/server/app/core/database.py
@@ -1,6 +1,8 @@
+from dotenv import load_dotenv
 import os
-from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+
+load_dotenv()
 
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
@@ -10,7 +12,7 @@ DB_NAME = os.getenv("DB_NAME")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"mysql+aiomysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_async_engine(
     DATABASE_URL,
