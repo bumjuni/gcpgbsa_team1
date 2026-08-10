13bede1dee3c6bfe892fe65d5991fb58528671db
JJy <darlian0889@gmail.com>
2026-08-10 09:23:59 +0900

    chore(be): add in-memory DB for demonstration

--- apps/server/app/api/dependencies.py
+++ apps/server/app/api/dependencies.py
@@ -6,13 +6,29 @@ from core.database import async_session_factory
 from services.classroom import ClassroomService
 
 
-async def get_db() -> AsyncGenerator[AsyncSession, None]:
-    yield None
+# async def get_db() -> AsyncGenerator[AsyncSession, None]:
     # async with async_session_factory() as session:
     #     yield session
 
 
+# def get_classroom_service(
+#     db: AsyncSession = Depends(get_db),
+# ) -> ClassroomService:
+#     return ClassroomService(db=db)
+
+from core.database import FakeAsyncSession
+from services.classroom import ClassroomService
+
+# 메모리 저장소 사용
+async def get_db() -> AsyncGenerator[FakeAsyncSession, None]:
+    session = FakeAsyncSession()
+    try:
+        yield session
+    finally:
+        await session.close()
+
+
 def get_classroom_service(
-    db: AsyncSession = Depends(get_db),
+    db: FakeAsyncSession = Depends(get_db),
 ) -> ClassroomService:
     return ClassroomService(db=db)
