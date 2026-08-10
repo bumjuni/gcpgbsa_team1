21924c3ec8b71c91baa46a5046be6dd422f028ba
JJy <darlian0889@gmail.com>
2026-08-10 17:06:06 +0900

    fix(be): modify CloudSQL mySQL connection config & SqlAlchemy Base

--- apps/server/app/core/database.py
+++ apps/server/app/core/database.py
@@ -1,11 +1,17 @@
 from dotenv import load_dotenv
 import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+import ssl
+
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
 
 load_dotenv()
 
 DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
 DB_NAME = os.getenv("DB_NAME")
 
 # CloudSQL IP
@@ -14,13 +20,18 @@ DB_PORT = os.getenv("DB_PORT")
 
 DATABASE_URL = f"mysql+aiomysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
+Base = declarative_base()
+
 engine = create_async_engine(
     DATABASE_URL,
     echo=False,
     pool_size=5,        # 커넥션 풀 설정 (운영 환경 필수)
     max_overflow=10,
-    pool_pre_ping=True  # 끊어진 DB 연결 자동 재시도
-)
+    pool_pre_ping=True,  # 끊어진 DB 연결 자동 재시도
+    connect_args={
+            "ssl": ssl_context,
+            "connect_timeout": 5,
+        },)
 
 AsyncSessionLocal = async_sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False
