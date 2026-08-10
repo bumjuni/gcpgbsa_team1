8e8e66fd889497369dbe7f2ee8a01915fff27caf
JJy <darlian0889@gmail.com>
2026-08-10 11:03:46 +0900

    [#4] feat(fe): add navigation from ClassListScreen to classRegisterScreen

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -17,16 +17,35 @@ import { FormField } from './src/components/FormField';
 import { ConfirmModal } from './src/components/ConfirmModal';
 import { ClassListScreen } from './src/screens/ClassListScreen';
 import { ClassListScreen_Filled } from './src/screens/ClassListScreen_Filled';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ClassRegisterScreen } from './src/screens/ClassRegisterScreen';
 
 export default function App() {
   const isDarkMode = useColorScheme() === 'dark';
+  const Stack = createNativeStackNavigator();
 
   return (
-    <NavigationContainer>
-      <SafeAreaProvider style={{flex: 1}}>
-        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-        <ClassListScreen />
-      </SafeAreaProvider>
-    </NavigationContainer>
+    <SafeAreaProvider style={{flex: 1, backgroundColor: '#FFFFFF'}}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+
+      <NavigationContainer>
+        <Stack.Navigator
+            initialRouteName="ClassList"
+          screenOptions={{
+            headerShown: false,
+            contentStyle: {backgroundColor: '#FFFFFF'}
+            }}>
+          <Stack.Screen
+            name="ClassList"
+            component={ClassListScreen}
+          />
+          <Stack.Screen
+            name="ClassRegister"
+            component={ClassRegisterScreen}
+            />
+          </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+
   );
 }
