7358e739055019464f9a32b4a680697173a5023e
JJy <darlian0889@gmail.com>
2026-08-11 10:12:28 +0900

    feat(fe): add classroom api

--- apps/mobile/src/api/classroom.ts
+++ apps/mobile/src/api/classroom.ts
@@ -1,21 +1,32 @@
 import { apiClient } from "./client"
-
-export interface SwimClass {
-  id: number;
-  name: string;
-  capacity: number;
-  level: string;
-  age_groups: string;
-  duration_min: number;
-  is_active: boolean;
-  status: boolean;
-}
+import { SwimClass } from "./types";
 
 export const classroomApi = {
   // 강습 목록 조회
-  getClasses: async (class_id: number) => {
-    const response = await apiClient.get<SwimClass[]>(`/classroom/classes/${class_id}`);
+  getClasses: async () => {
+    const response = await apiClient.get<SwimClass[]>(`/classroom`);
     console.log(response);
     return response.data;
-  }
+  },
+
+  // 강습 상세정보 조회
+  getClassDetail: async (class_id: number) => {
+    const response = await apiClient.get<SwimClass[]>(`/classroom/${class_id}`);
+    console.log(response);
+    return response.data;
+  },
+
+  // 새로운 강습반 등록
+  createClass: async (classData: SwimClass) => {
+      const response = await apiClient.post<SwimClass>('/classroom', classData);
+      console.log(response);
+      return response.data;
+  },
+
+  // 강습반 삭제
+  deleteClass: async (class_id: number) => {
+      const response = await apiClient.delete<SwimClass>(`/classroom/${class_id}`);
+      console.log(response);
+      return response.data;
+  },
 }
